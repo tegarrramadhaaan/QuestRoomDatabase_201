9a2efc8a553956badebe4b9bbac6680f87d4b29b
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-02 14:33:52 +0700

    menambahkan code Siswa.kt

--- app/src/main/java/com/example/myroomsatu/room/Siswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/Siswa.kt
@@ -1,2 +1,13 @@
 package com.example.myroomsatu.room
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class  Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
