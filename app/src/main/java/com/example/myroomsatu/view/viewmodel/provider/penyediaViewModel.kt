0ce2361423764a4f13fa80064353240301c0807c
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-02 14:48:14 +0700

    menambahkan code penyediaViewModel.kt

--- app/src/main/java/com/example/myroomsatu/view/viewmodel/provider/penyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/view/viewmodel/provider/penyediaViewModel.kt
@@ -30,5 +30,3 @@ object PenyediaViewModel {
  * Fungsi ekstensi untuk mendapatkan instance AplikasiSiswa dari CreationExtras.
  * Ini adalah cara standar untuk mengakses Application context dalam ViewModel factory.
  */
-fun CreationExtras.aplikasiSiswa(): AplikasiSiswa = 
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
