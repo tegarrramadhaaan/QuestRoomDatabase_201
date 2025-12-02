b553a9c2ddc3519237acbffd3f665cf9cda9342e
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-02 14:31:59 +0700

    menambahkan code destinasi entry.kt

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiNavigasi.kt
@@ -1,13 +1,2 @@
 package com.example.myroomsatu.view.route
 
-interface DestinasiNavigasi {
-    /**
-     * Nama unik untuk menentukan jalur untuk composable
-     */
-    val route: String
-
-    /**
-     * String resource id yang berisi judul yang akan ditampilkan di layar halaman.
-     */
-    val titleRes: String
-}
